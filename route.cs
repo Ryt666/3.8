1e99ded889b968b0b97e191fbdc8e861fb12866a
Ryt666 <96390850+Ryt666@users.noreply.github.com>
2021-12-20 11:40:57 +0300

    Create route.cs

---
+++ route.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3._8
+{
+    class Route : Sensor
+    { 
+        public Route() : base("Направление ветра:","северо-восточное")
+        {
+    
+        }
+
+
+    }
+}
