451209fe73c20832565fce3b2ed915aac35b3786
Ryt666 <96390850+Ryt666@users.noreply.github.com>
2021-12-20 11:42:49 +0300

    Create center_post.cs

---
+++ center_post.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3._8
+{
+    class Center_post
+    {
+        public void CheckInfo(Sensor sensor)
+        {
+            sensor.ShowInfo();
+        }
+        //класс может получать данные из всех датчиков, которые унаследованы от сенсора 
+    }
+}
